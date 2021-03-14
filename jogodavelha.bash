665a15290004972cc0496f9af329e09572c68d38
felipaoasmoura <felipaoasmoura@gmail.com>
2021-03-14 20:40:24 -0300

    First commit

--- jogodavelha.bash
+++ jogodavelha.bash
@@ -1,35 +1,24 @@
 #!/bin/bash
-clear;
-echo '';
-echo '   |   |';
-echo '---+---+---';
-echo '   |   | ';
-echo '---+---+---';
-echo '   |   | ';
-echo '';
-read -p 'Qual casa você quer o "X" ' x;
-echo '';
-read -p 'Qual casa quer o "O" ' o;
+
+'''
+Program: Jogo da Velha
+Author: Felipe Augusto de Souza Moura
+Created at: 2021/03/10
+'''
+
+declare -r O="O";
+declare -r X="X";
 
 clear;
-O="O";
-X="X";
+
 echo '';
-echo " $X |   |   ";
+echo '   |   |   ';
 echo '---+---+---';
-echo "   | $O |   ";
+echo '   |   |   ';
 echo '---+---+---';
 echo '   |   |   ';
 echo '';
+
 read -p 'Qual casa você quer o "X" ' x;
 echo '';
 read -p 'Qual casa quer o "O" ' o;
-
-clear;
-echo '';
-echo " $X |   |   ";
-echo '---+---+---';
-echo "   | $O | $O ";
-echo '---+---+---';
-echo "   |   | $X ";
-echo '';
