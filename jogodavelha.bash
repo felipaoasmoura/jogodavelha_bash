2e105ee6c508023dcbe1b3da47d350422b31745f
felipaoasmoura <felipaoasmoura@gmail.com>
2021-03-15 19:58:30 -0300

    90% da primeira versão

--- jogodavelha.bash
+++ jogodavelha.bash
@@ -10,21 +10,84 @@ set -e
 declare -r O="O";
 declare -r X="X";
 vez=$(( $RANDOM % 2 ));
-clear;
+a1=' ';
+a2=' ';
+a3=' ';
+b1=' ';
+b2=' ';
+b3=' ';
+c1=' ';
+c2=' ';
+c3=' ';
+contador=0;
 
-echo "";
-echo "   |   |   ";
-echo "---+---+---";
-echo "   |   |   ";
-echo "---+---+---";
-echo "   |   |   ";
-echo "";
+while [[ $contador -lt 9 ]];
+do
+  contador=$(( $contador + 1 ));
+  clear;
 
 
-if [[ $vez -eq 0 ]];
-then
-  read -p "Qual casa você quer o "X" " x;
-else
-  read -p "Qual casa quer o "O" " o;
-fi
-echo "";
+  echo "";
+  echo " $a1 | $a2 | $a3 ";
+  echo "---+---+---";
+  echo " $b1 | $b2 | $b3 ";
+  echo "---+---+---";
+  echo " $c1 | $c2 | $c3 ";
+  echo "";
+
+  if [[ $a1 != " " && $a1 == $a2 && $a3 == $a1 ]];
+  then
+    echo "Parabéns, jogador $a1";
+    exit;
+  fi
+  if [[ $b1 != " " && $b1 == $b2 && $b3 == $b1 ]];
+  then
+    echo "Parabéns, jogador $b1";
+    exit;
+  fi
+  if [[ $c1 != " " && $c1 == $c2 && $c3 == $c1 ]];
+  then
+    echo "Parabéns, jogador $c1";
+    exit;
+  fi
+
+  if [[ $a1 != " " && $a1 == $b1 && $c1 == $a1 ]];
+  then
+    echo "Parabéns, jogador $a1";
+    exit;
+  fi
+  if [[ $a2 != " " && $a2 == $b2 && $c2 == $a2 ]];
+  then
+    echo "Parabéns, jogador $a2";
+    exit;
+  fi
+  if [[ $a3 != " " && $a3 == $b3 && $c3 == $a3 ]];
+  then
+    echo "Parabéns, jogador $a3";
+    exit;
+  fi
+
+    if [[ $a1 != " " && $a1 == $b2 && $c3 == $a1 ]];
+    then
+      echo "Parabéns, jogador $a1";
+      exit;
+    fi
+    if [[ $a3 != " " && $c1 == $b2 && $c1 == $a3 ]];
+    then
+      echo "Parabéns, jogador $a3";
+      exit;
+    fi
+
+  if [[ $vez -eq 0 ]];
+  then
+    read -p "Qual casa você quer o 'X' " x;
+    eval "$x=$X";
+    vez=1;
+  else
+    read -p "Qual casa quer o 'O' " o;
+    o=${o,,};
+    eval "$o=$O";
+    vez=0;
+  fi
+  echo "";
+done
