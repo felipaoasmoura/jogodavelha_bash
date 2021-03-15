46c32010c65530de57d016c977a16c569b58dda9
felipaoasmoura <felipaoasmoura@gmail.com>
2021-03-15 13:39:31 -0300

    aprendi o random

--- jogodavelha.bash
+++ jogodavelha.bash
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -e
 
-'''
-Program: Jogo da Velha
-Author: Felipe Augusto de Souza Moura
-Created at: 2021/03/10
-'''
+#```
+#Program: Jogo da Velha
+#Author: Felipe Augusto de Souza Moura
+#Created at: 2021/03/10
+#```
 
 declare -r O="O";
 declare -r X="X";
-
+vez=$(( $RANDOM % 2 ));
 clear;
 
 echo "";
@@ -20,6 +20,11 @@ echo "---+---+---";
 echo "   |   |   ";
 echo "";
 
-read -p "Qual casa você quer o "X" " x;
+
+if [[ $vez -eq 0 ]];
+then
+  read -p "Qual casa você quer o "X" " x;
+else
+  read -p "Qual casa quer o "O" " o;
+fi
 echo "";
-read -p "Qual casa quer o "O" " o;
