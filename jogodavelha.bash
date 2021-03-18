255b542dd7e3731022a15aeab4ddfa8a6b275bce
felipaoasmoura <felipaoasmoura@gmail.com>
2021-03-18 20:58:32 -0300

    função

--- jogodavelha.bash
+++ jogodavelha.bash
@@ -1,14 +1,13 @@
 #!/bin/bash
-set -e
-
+#set -x
+set -o errexit   # abort on nonzero exitstatus
+set -o nounset   # abort on unbound variable
+set -o pipefail  # don't hide errors within pipes
 #```
 #Program: Jogo da Velha
 #Author: Felipe Augusto de Souza Moura
 #Created at: 2021/03/10
 #```
-
-
-
 declare -r O="O";
 declare -r X="X";
 vez=$(( $RANDOM % 2 ));
@@ -27,10 +26,9 @@ vermelho='\033[31m';
 padrao='\033[0m';
 azul='\033[34m';
 campeao='\033[5;33m';
-while [[ $contador -lt 9 ]];
-do
-  contador=$(( $contador + 1 ));
 
+function verifica_vencedor() {
+  contador=$(( $contador + 1 ));
   if [[ $a1 != " " && $a1 == $a2 && $a3 == $a1 ]];
   then
     vencedor="Parabéns, jogador $a1";
@@ -52,7 +50,6 @@ do
     c2=$campeao$c2$padrao
     c3=$campeao$c3$padrao
   fi
-
   if [[ $a1 != " " && $a1 == $b1 && $c1 == $a1 ]];
   then
     vencedor="Parabéns, jogador $a1";
@@ -74,50 +71,72 @@ do
     b3=$campeao$b3$padrao
     c3=$campeao$c3$padrao
   fi
-
-    if [[ $a1 != " " && $a1 == $b2 && $c3 == $a1 ]];
-    then
-      vencedor="Parabéns, jogador $a1";
-      a1=$campeao$a1$padrao
-      b2=$campeao$b2$padrao
-      c3=$campeao$c3$padrao
-    fi
-    if [[ $a3 != " " && $c1 == $b2 && $c1 == $a3 ]];
-    then
-      vencedor="Parabéns, jogador $a3";
-      a3=$campeao$a3$padrao
-      b2=$campeao$b2$padrao
-      c1=$campeao$c1$padrao
-    fi
-
-    clear;
-
-    echo "     1   2   3";
-    echo "";
-    echo -e " a   $a1 | $a2 | $a3 ";
-    echo "    ---+---+---";
-    echo -e " b   $b1 | $b2 | $b3 ";
-    echo "    ---+---+---";
-    echo -e " c   $c1 | $c2 | $c3 ";
-    echo "";
-
-  if [[ -n $vencedor ]];
+  if [[ $a1 != " " && $a1 == $b2 && $c3 == $a1 ]];
   then
-    echo $vencedor;
-    exit;
+    vencedor="Parabéns, jogador $a1";
+    a1=$campeao$a1$padrao
+    b2=$campeao$b2$padrao
+    c3=$campeao$c3$padrao
   fi
+  if [[ $a3 != " " && $c1 == $b2 && $c1 == $a3 ]];
+  then
+    vencedor="Parabéns, jogador $a3";
+    a3=$campeao$a3$padrao
+    b2=$campeao$b2$padrao
+    c1=$campeao$c1$padrao
+  fi
+}
+
+function desenha_tabulero() {
+  clear;
+  echo "     1   2   3";
+  echo "";
+  echo -e " a   $a1 | $a2 | $a3 ";
+  echo "    ---+---+---";
+  echo -e " b   $b1 | $b2 | $b3 ";
+  echo "    ---+---+---";
+  echo -e " c   $c1 | $c2 | $c3 ";
+  echo "";
+}
+desenha_tabulero;
+while [[ $contador -lt 9 ]];
+do
+
 
   if [[ $vez -eq 0 ]];
   then
-    read -p "Qual casa você quer o 'X' " x;
+    livre="s";
+    while [[ -n $livre ]];
+    do
+      read -p "Qual casa você quer o 'X' " x;
+      x=${x,,};
+      livre="$(echo ${!x} | tr -d '\ ')";
+    done
     eval "$x=$X";
     vez=1;
   else
-    read -p "Qual casa quer o 'O' " o;
-    o=${o,,};
+    livre="s";
+    while [[ -n $livre ]];
+    do
+      read -p "Qual casa quer o 'O' " o;
+      o=${o,,};
+      livre="$(echo ${!o} | tr -d '\ ')";
+    done
     eval "$o=$O";
     vez=0;
   fi
   echo "";
+
+    verifica_vencedor;
+    desenha_tabulero;
+
+    if [[ -n $vencedor ]];
+    then
+      echo $vencedor;
+      exit;
+    fi
+
 done
-echo 'A velha ganhou'
+
+
+echo 'A velha ganhou';
